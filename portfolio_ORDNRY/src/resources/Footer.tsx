b7a70033f792eac79f4da4aed00bb24a840be596
Samuel Nogueira <samucans2004@gmail.com>
2026-01-15 02:11:08 -0300

    Adicionei footer

--- portfolio_ORDNRY/src/resources/Footer.tsx
+++ portfolio_ORDNRY/src/resources/Footer.tsx
@@ -1,11 +1,37 @@
 import "./Footer-Style.css";
+import Button from "./Button";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <p className="footer-text">
+    <footer className="footer-container">
+      <div className="footer-content-container">
+        <div className="footer-copy-container">
+          <img src="./logo.svg" alt="ORDNRY Logo" className="footer-logo" />
+          <p className="color-neutral-100 body-16-medium">
+            Crio soluções digitais que equilibram design e usabilidade,
+            transformando a interação em uma jornada fluida e significativa.
+          </p>
+          <Button text="BAIXAR CURRÍCULO" />
+        </div>
+
+        <div className="footer-links-container">
+          <div className="footer-links-column">
+            <a>Projetos</a>
+            <a>Sobre</a>
+            <a>Carreira </a>
+          </div>
+          <div className="footer-links-column"></div>
+        </div>
+      </div>
+      <div className="linha-footer"></div>
+      <p className="color-neutral-000 body-16-medium">
         &copy; {new Date().getFullYear()} ORDNRY. Todos os direitos reservados.
       </p>
+      <img
+        src="./ordnry-overlay-footer.svg"
+        alt="ORDNRY Footer Overlay"
+        className="footer-overlay"
+      />
     </footer>
   );
 }

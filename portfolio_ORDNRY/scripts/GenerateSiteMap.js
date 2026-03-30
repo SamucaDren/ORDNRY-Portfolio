44486ea0aee881bb1e491f7fb67390f1eaddd3da
Samuel Nogueira <samucans2004@gmail.com>
2026-03-30 00:58:33 -0300

    adicionei sitemap

---
+++ portfolio_ORDNRY/scripts/GenerateSiteMap.js
@@ -0,0 +1,33 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+import { writeFileSync } from "fs";
+
+const links = [{ url: "/" }, { url: "/front-end" }];
+
+const stream = new SitemapStream({
+  hostname: "https://ordnrydesign.com",
+});
+
+(async () => {
+  try {
+    links
+      .filter((l) => l.url && typeof l.url === "string")
+      .forEach((l) => {
+        stream.write({
+          url: l.url.trim(),
+          changefreq: "weekly",
+          priority: l.url === "/" ? 1.0 : 0.8,
+          lastmod: new Date().toISOString(), // 🔹 aqui
+        });
+      });
+
+    stream.end();
+
+    const data = await streamToPromise(stream).then((res) => res.toString());
+
+    writeFileSync("./public/sitemap.xml", data);
+
+    console.log("Sitemap gerado com sucesso!");
+  } catch (err) {
+    console.error("Erro:", err);
+  }
+})();
